84e7a497e2c2e928daa908f808a1b751ed3dbc95
Andrey Belchikov <belchikovs@gmail.com>
2018-08-14 15:50:31 +0300

    Removed unused code

--- screens/CallsScreen.js
+++ screens/CallsScreen.js
@@ -3,24 +3,12 @@ import {
   ScrollView,
   StyleSheet,
   View,
-  Text
 } from 'react-native';
 import {Endpoint} from 'react-native-pjsip'
 
-debugger;
 const endpoint = new Endpoint();
-console.log(endpoint);
-endpoint.start().then(state => {
-  debugger;
-  console.log(state);
-
-}, console.error)
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
   render() {
     return (
       <View style={styles.container}>
