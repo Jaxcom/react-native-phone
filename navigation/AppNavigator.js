c71156d11249fc7c5d34b94c497ea038672b68f1
Andrey Belchikov <belchikovs@gmail.com>
2018-08-16 17:43:08 +0300

    Small ui fixes

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,13 +4,13 @@ import MainTabNavigator from './MainTabNavigator';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen'
 import SignInScreen from '../screens/SignInScreen'
 
-const AppStack = createStackNavigator({ Main: MainTabNavigator });
 const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
 export default createSwitchNavigator({
-  App: AppStack,
+  App: MainTabNavigator,
   Auth: AuthStack,
   AuthLoading: AuthLoadingScreen,
 }, {
   initialRouteName: 'AuthLoading',
 });
+
--- screens/CallsScreen.js
+++ screens/CallsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   ScrollView,
   View,
+  Text
 } from 'react-native';
 import {Endpoint} from 'react-native-pjsip';
 import styles from '../constants/Styles';
@@ -9,10 +10,15 @@ import styles from '../constants/Styles';
 const endpoint = new Endpoint();
 
 export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Calls',
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <Text>TODO: add dialer</Text>
         </ScrollView>
       </View>
     );
