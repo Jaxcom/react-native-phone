bae31dbf5541864e072456111115e64bf6b6ac7d
Andrey Belchikov <belchikovs@gmail.com>
2018-08-17 15:09:35 +0300

    Reimplemented ligin ui with another ui lib

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,121 +1,95 @@
 import React from 'react';
 import {SecureStore} from 'expo';
-import validate from 'validate.js'
+import t from 'tcomb-form-native'
 
 import {
     Button,
     View,
     AsyncStorage,
+    ActivityIndicator,
+    StyleSheet
 } from 'react-native';
 
-import TextField from '../components/TextField';
-import styles from '../constants/Styles';
+const Form = t.form.Form;
+
+const SignIn = t.struct({
+    userId: t.String,
+    apiToken: t.String,
+    apiSecret: t.String,
+    domainId: t.maybe(t.String),
+    phoneNumber: t.maybe(t.String),
+    baseUrl: t.String,
+});
+
+const options = {
+    fields: {
+        userId: {
+            label: 'User ID'
+        },
+        domainId: {
+            label: 'Domain ID (optional)',
+            help: 'Leave empty if you would like to create new SIP domain'
+        },
+        phoneNumber: {
+            help: 'Leave empty if you would like to reserve new phone number'
+        },
+        baseUrl: {
+            label: 'Base url to backend server'
+        }
+    }
+};
+
+const defaultValues = {
+    baseUrl: 'https://some-server',
+};
 
 export default class SignInScreen extends React.Component {
     static navigationOptions = {
         title: 'Log in',
     };
 
+    state = {
+        inProgress: false
+    }
+
     render() {
         return (
         <View style={styles.container}>
-            <TextField
-                autoFocus={true}
-                placeholder="User ID"
-                onChangeText={text => this.setState({userId: text})}
-                error={this.state.errors.userId}
+            <Form
+                ref="form"
+                type={SignIn}
+                options={options}
+                value={defaultValues}
             />
-            <TextField
-                placeholder="Api Token"
-                onChangeText={text => this.setState({apiToken: text})}
-                error={this.state.errors.apiToken}
-            />
-            <TextField
-                placeholder="Api Secret"
-                onChangeText={text => this.setState({apiSecret: text})}
-                error={this.state.errors.apiSecret}
-            />
-            <TextField
-                placeholder="SIP Domain ID"
-                onChangeText={text => this.setState({domainId: text})}
-                error={this.state.errors.domainId}
-                hint="Leave empty to create new SIP domain automatically."
-            />
-            <TextField
-                placeholder="Phone Number (like +11234567890)"
-                keyboardType="phone-pad"
-                onChangeText={text => this.setState({phoneNumber: text})}
-                error={this.state.errors.phoneNumber}
-                hint="Leave empty if you would like to allocate new number."
-            />
-            <TextField
-                placeholder="Backend Server URL"
-                onChangeText={text => this.setState({baseUrl: text})}
-                error={this.state.errors.baseUrl}
-            />
-            <Button title="Sign in" onPress={this._signInAsync} disabled={this.state.inProgress} />
+            <Button title="Log in" onPress={this._logInAsync} disabled={this.state.inProgress} />
             <ActivityIndicator animating={this.state.inProgress}/>
         </View>
         );
     }
 
-    _validate = async () => {
-        const result = await validate.async(this.state, {
-            userId: {
-                presence: true
-            },
-            apiToken: {
-                presence: true
-            },
-            apiSecret: {
-                presence: true
-            },
-            domainId: {
-                presence: true
-            },
-            phoneNumber: {
-                length: {
-                    is: 6,
-                }
-            },
-            baseUrl: {
-                presence: true
-            },
-        })
-        const errors = {}
-        let hasErrors = false
-        Object.keys(result || {}).forEach(key => {
-            errors[key] = result[key].join(', ')
-            hasErrors = true
-        })
-        this.setState({errors});
-        return hasErrors;
-    }
-
-    _signInAsync = async () => {
+    _logInAsync = async () => {
         this.setState({inProgress: true, error: null})
         try {
-            const hasErrors = await this._validate();
-            if (hasErrors) {
+            const data = this.refs.form.getValue();
+            if (!data) {
                 return;
             }
-            let baseUrl = this.state.baseUrl;
+            let {baseUrl} = data;
             if (baseUrl.endsWith('/')) {
                 baseUrl = baseUrl.substr(0, baseUrl.length - 1)
             }
-            const {userId, apiToken, apiSecret, phoneNumber, sipUserName, sipPassword} = this.state;
             const response = await await fetch(`${baseUrl}/login`, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({userId, apiToken, apiSecret, phoneNumber, sipUserName, sipPassword}),
+                body: JSON.stringify(data),
             });
             if (response.ok) {
                 const {sipUri, password, phoneNumber} = await response.json();
                 await SecureStore.setItemAsync('sip', JSON.stringify({sipUri, password}));
-                await AsyncStorage.setItem('userId', userId);
+                await AsyncStorage.setItem('userId', data.userId);
                 await AsyncStorage.setItem('phoneNumber', phoneNumber);
                 await AsyncStorage.setItem('baseUrl', baseUrl);
             } else {
@@ -130,3 +104,11 @@ export default class SignInScreen extends React.Component {
         }
     };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 4
+    }
+});
