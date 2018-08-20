0cf42ec7416723b10ca6a942c3426180e68c5add
Andrey Belchikov <belchikovs@gmail.com>
2018-08-20 16:08:37 +0300

    Started implementation of dialer

--- screens/CallsScreen.js
+++ screens/CallsScreen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {
   ScrollView,
   View,
-  Text
+  StyleSheet,
+  Button,
+  Text,
 } from 'react-native';
 import {Endpoint} from 'react-native-pjsip';
 import styles from '../constants/Styles';
@@ -13,14 +15,66 @@ export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Calls',
   };
+  
+  state = {
+    phoneNumber: ''
+  }
+
+  pressButton(digit){
+    return () => this.setState({phoneNumber: `${this.state.phoneNumber}${digit}`})
+  }
+
+  makeCall() {
+
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <Text>TODO: add dialer</Text>
+          <View style={callsStyles.dialer}>
+            <Text>{this.state.phoneNumber}</Text>
+          </View>
+          <View style={callsStyles.dialer}>
+            <Button title="1" style={callsStyles.button} onPress={this.pressButton('1')}/>
+            <Button title="2" style={callsStyles.button} onPress={this.pressButton('2')}/>
+            <Button title="3" style={callsStyles.button} onPress={this.pressButton('3')}/>
+          </View>
+          <View style={callsStyles.dialer}>
+            <Button title="4" style={callsStyles.button} onPress={this.pressButton('4')}/>
+            <Button title="5" style={callsStyles.button} onPress={this.pressButton('5')}/>
+            <Button title="6" style={callsStyles.button} onPress={this.pressButton('6')}/>
+          </View>
+          <View style={callsStyles.dialer}>
+            <Button title="7" style={callsStyles.button} onPress={this.pressButton('7')}/>
+            <Button title="8" style={callsStyles.button} onPress={this.pressButton('8')}/>
+            <Button title="9" style={callsStyles.button} onPress={this.pressButton('9')}/>
+          </View>
+          <View style={callsStyles.dialer}>
+            <Button title="*" style={callsStyles.button} onPress={this.pressButton('*')}/>
+            <Button title="0" style={callsStyles.button} onPress={this.pressButton('0')}/>
+            <Button title="#" style={callsStyles.button} onPress={this.pressButton('#')}/>
+          </View>
+          <View style={callsStyles.dialer}>
+            <Button title="Call" style={callsStyles.callButton} onPress={this.makeCall}/>
+          </View>
         </ScrollView>
       </View>
     );
   }
 }
+
+const callsStyles = StyleSheet.create({
+  dialer: {
+    flex: 1, 
+    flexDirection:'row', 
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button: {
+    width: "100%"
+  },
+  callButton: {
+    alignSelf: 'stretch' 
+  }
+});
